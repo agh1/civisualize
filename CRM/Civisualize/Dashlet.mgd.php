646053747ffbe1f50adfc5a4de5bd036bf75e74b
Andrew Hunt <andrew@aghstrategies.com>
2015-09-14 19:11:19 -0400

    add dashlets

--- CRM/Civisualize/Dashlet.mgd.php
+++ CRM/Civisualize/Dashlet.mgd.php
@@ -1,37 +1,54 @@
 <?php
 
-return array (
-0=>array (
-  'name'=>'dataviz_contact',
-  'entity'=> 'Dashboard',
-  'params'=> array (
-    'version'=>'3',
-    'name'=>'dataviz_contact',
-    'label'=> 'Dataviz of contacts',
-    'url' => CRM_Utils_System::url('civicrm/dataviz/contacts', 'snippet=4'),
-  )
-),
-
-1=>array (
-  'name'=>'dataviz_events',
-  'entity'=> 'Dashboard',
-  'params'=> array (
-    'version'=>'3',
-    'name'=>'dataviz_event',
-    'label'=> 'Dataviz of events',
-    'url' => CRM_Utils_System::url('civicrm/dataviz/events', 'snippet=4'),
-  )
-),
-
-2=>array (
-  'name'=>'dataviz_contribute',
-  'entity'=> 'Dashboard',
-  'params'=> array (
-    'version'=>'3',
-    'name'=>'dataviz_contribute',
-    'label'=> 'Dataviz of contributions',
-    'url' => CRM_Utils_System::url('civicrm/dataviz/contribute', 'snippet=4'),
-  )
-)
-
+return array(
+  0 => array(
+    'name' => 'dataviz_contact',
+    'entity' => 'Dashboard',
+    'params' => array(
+      'version' => '3',
+      'name' => 'dataviz_contact',
+      'label' => 'Dataviz of contacts',
+      'url' => CRM_Utils_System::url('civicrm/dataviz/contacts', 'snippet=4'),
+    ),
+  ),
+  1 => array(
+    'name' => 'dataviz_events',
+    'entity' => 'Dashboard',
+    'params' => array(
+      'version' => '3',
+      'name' => 'dataviz_event',
+      'label' => 'Dataviz of events',
+      'url' => CRM_Utils_System::url('civicrm/dataviz/events', 'snippet=4'),
+    ),
+  ),
+  2 => array(
+    'name' => 'dataviz_contribute',
+    'entity' => 'Dashboard',
+    'params' => array(
+      'version' => '3',
+      'name' => 'dataviz_contribute',
+      'label' => 'Dataviz of contributions',
+      'url' => CRM_Utils_System::url('civicrm/dataviz/contribute', 'snippet=4'),
+    ),
+  ),
+  3 => array(
+    'name' => 'dataviz_member',
+    'entity' => 'Dashboard',
+    'params' => array(
+      'version' => '3',
+      'name' => 'dataviz_member',
+      'label' => 'Dataviz of memberships',
+      'url' => CRM_Utils_System::url('civicrm/dataviz/member', 'snippet=4'),
+    ),
+  ),
+  4 => array(
+    'name' => 'dataviz_historicalMember',
+    'entity' => 'Dashboard',
+    'params' => array(
+      'version' => '3',
+      'name' => 'dataviz_historicalMember',
+      'label' => 'Dataviz of historical memberships',
+      'url' => CRM_Utils_System::url('civicrm/dataviz/historicalMember', 'snippet=4'),
+    ),
+  ),
 );
